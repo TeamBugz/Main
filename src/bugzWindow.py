6b9e763180a07077b2fac14d9fe1f0e1dbbf6f99
Miguel Salvador <miguelsalvador355@gmail.com>
2021-11-03 15:54:12 -0400

    created a window

--- src/bugzWindow.py
+++ src/bugzWindow.py
@@ -1,1 +1,20 @@
-print("hello World");
+import pygame
+
+#creating window
+background_colour = (245, 245, 245)
+(width, height) = (300, 200)
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Bugz World')
+
+
+screen.fill(background_colour)
+pygame.display.flip()
+
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
